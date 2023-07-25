dbb5a3796688db530beb0dd3f3454096bb643aaf
shikha dhar <shikhadhar212@gmail.com>
2023-07-25 12:24:16 +0530

    submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function removeOptions()
+{
+    const colorSelect=document.getElementById("colorSelect");
+    const selectedIndex=colorSelect.selectedIndex;
+    if(selectedIndex !==-1)
+    {
+        colorSelect.remove(selectedIndex);
+    }
+    
+}
